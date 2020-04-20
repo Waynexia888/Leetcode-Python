a0b95216c36b70b796b26e923e79ff945dfa3a0b
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-20 00:36:16 -0700

    updated

---
+++ 第二章 性价比之王的宽度优先搜索/4_图上的宽度优先搜索/简介.py
@@ -0,0 +1,11 @@
+# BFS 大部分的时候是在图上进行的。
+
+# 什么是图（Graph）
+# 图在离线数据中的表示方法为 < E, V >，E表示 Edge，V 表示 Vertex。也就是说，图是顶点（Vertex）和边（Edge）的集合。
+
+# 图分为：
+
+# 有向图（Directed Graph）
+# 无向图（Undirected Graph）
+
+# BFS 在两种图上都适用。另外，树（Tree）也是一种特殊的图。
