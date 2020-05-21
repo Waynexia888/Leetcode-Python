4844809727e7d5b5b475dde2d9e28810f61d9ca9
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-21 00:08:37 -0700

    updated

--- Lintcode/521. Remove Duplicate Numbers in Array.py
+++ Lintcode/521. Remove Duplicate Numbers in Array.py
@@ -7,25 +7,25 @@ class Solution:
     def deduplication(self, nums):
         # write your code here
         # 使用哈希表解决， 时间O(n), 空间O(n)
-        # dict = {}
-        # result = 0
-        # for num in nums:
-        #     if num not in dict:
-        #         dict[num] = True
-        #         nums[result] = num
-        #         result += 1
-
-        # return result
+        dict = {}
+        count = 0
+        for num in nums:
+            if num not in dict:
+                dict[num] = True
+                nums[count] = num
+                count += 1
+        return count
 
         # 使用双指针，O(nlogn) time, O(1) extra space
-        if len(nums) == 0:
-            return 0
+        # if not nums or len(nums) == 0:
+        #     return 0
+        # nums.sort()
 
-        nums.sort()
-        result = 1  # 慢指针，指向第二个数
-        for i in range(1, len(nums)):
-            if nums[i - 1] != nums[i]:
-                nums[result] = nums[i]
-                result += 1
+        # left, right = 0, 1
+        # while right < len(nums):
+        #     if nums[right] != nums[left]:
+        #         left += 1
+        #         nums[left] = nums[right]
+        #     right += 1
 
-        return result
+        # return left + 1
