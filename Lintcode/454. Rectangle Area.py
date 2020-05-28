a2ffc86fa8afebeb2c6a03e3f796fbf28cbcbba7
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-28 14:00:35 -0700

    updated

---
+++ Lintcode/454. Rectangle Area.py
@@ -0,0 +1,17 @@
+class Rectangle:
+
+    '''
+     * Define a constructor which expects two parameters width and height here.
+    '''
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    '''
+     * Define a public method `getArea` which can calculate the area of the
+     * rectangle and return.
+    '''
+
+    def getArea(self):
+        return self.width * self.height
