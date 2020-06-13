6d8198d1b08da8d7ae47c0bd29c9937b2bbdd334
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-12 23:06:33 -0700

    updated

--- Lintcode/609. Two Sum - Less than or equal to target.py
+++ Lintcode/609. Two Sum - Less than or equal to target.py
@@ -6,19 +6,20 @@ class Solution:
     """
 
     def twoSum5(self, nums, target):
-        # write your code here
-        length = len(nums)
-        if not nums or length < 2:
+        # 排序 + 双指针
+
+        if nums is None or len(nums) < 2:
             return 0
 
         nums.sort()
-        ans = 0
-        left, right = 0, length - 1
+
+        left, right = 0, len(nums) - 1
+        count = 0
         while left < right:
             if nums[left] + nums[right] <= target:
-                ans += right - left
+                count += right - left
                 left += 1
             else:
                 right -= 1
 
-        return ans
+        return count
