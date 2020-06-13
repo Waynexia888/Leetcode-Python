f3eef6d8a4be37189e1d0a79b5fdd71db4ac4d4c
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-13 01:16:21 -0700

    updated

--- Lintcode/457. Classical Binary Search.py
+++ Lintcode/457. Classical Binary Search.py
@@ -25,3 +25,30 @@ class Solution:
             return end
 
         return -1
+
+##############################################################
+
+
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+
+    def findPosition(self, nums, target):
+        # 用递归的方法实现二分法
+        return self.binarySearch(nums, 0, len(nums) - 1, target)
+
+    def binarySearch(self, nums, start, end, target):
+        if start > end:
+            return -1
+
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[mid] < target:
+            return self.binarySearch(nums, mid + 1, end, target)
+
+        return self.binarySearch(nums, start, mid - 1, target)
