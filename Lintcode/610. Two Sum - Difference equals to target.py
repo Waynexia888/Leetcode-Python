ea5cad24b46ada6dc1013f98ab7d62869f1362fb
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-14 01:27:07 -0700

    updated

--- Lintcode/610. Two Sum - Difference equals to target.py
+++ Lintcode/610. Two Sum - Difference equals to target.py
@@ -1,3 +1,45 @@
+# class Solution:
+#     """
+#     @param nums: an array of Integer
+#     @param target: an integer
+#     @return: [num1, num2] (num1 < num2)
+#     """
+
+#     def twoSum7(self, nums, target):
+#         # 二分法解法, 时间复杂度O(nlogn), 空间复杂度O(1)
+
+#         if nums is None or len(nums) < 2:
+#             return [-1, -1]
+
+#         target = abs(target)
+#         for i in range(len(nums)):
+#             j = self.binarySearch(nums, i + 1, len(nums) - 1, target + nums[i])
+#             if j != -1:
+#                 return [nums[i], nums[j]]
+#         return [-1, -1]
+
+#     def binarySearch(self, nums, start, end, target):
+
+#         while start + 1 < end:
+#             mid = (start + end) // 2
+#             if nums[mid] == target:
+#                 return mid
+
+#             if nums[mid] < target:
+#                 start = mid
+#             else:
+#                 end = mid
+
+#         if nums[start] == target:
+#             return start
+#         if nums[end] == target:
+#             return end
+
+#         return -1
+
+####################################################
+
+
 class Solution:
     """
     @param nums: an array of Integer
@@ -6,33 +48,20 @@ class Solution:
     """
 
     def twoSum7(self, nums, target):
-        # 二分法解法, 时间复杂度O(nlogn), 空间复杂度O(1)
+        # 同向双指针的模版
 
         if nums is None or len(nums) < 2:
             return [-1, -1]
 
         target = abs(target)
+        j = 1
         for i in range(len(nums)):
-            j = self.binarySearch(nums, i + 1, len(nums) - 1, target + nums[i])
-            if j != -1:
+            j = max(j, i + 1)
+            while j < len(nums) and nums[j] - nums[i] < target:
+                j += 1
+            if j >= len(nums):
+                break
+
+            if nums[j] - nums[i] == target:
                 return [nums[i], nums[j]]
         return [-1, -1]
-
-    def binarySearch(self, nums, start, end, target):
-
-        while start + 1 < end:
-            mid = (start + end) // 2
-            if nums[mid] == target:
-                return mid
-
-            if nums[mid] < target:
-                start = mid
-            else:
-                end = mid
-
-        if nums[start] == target:
-            return start
-        if nums[end] == target:
-            return end
-
-        return -1
