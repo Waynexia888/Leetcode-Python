741d2120b254b7396a1edd3985341876b296520c
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-18 17:07:44 -0700

    updated

---
+++ Lintcode/644. Strobogrammatic Number.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param num: a string
+    @return: true if a number is strobogrammatic or false
+    """
+
+    def isStrobogrammatic(self, num):
+        # python 中: 检验key是否存在在dict里？ print 'a' in d
+
+        dict = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
+
+        left, right = 0, len(num) - 1
+        while left <= right:
+            if not num[left] in dict or dict[num[left]] != num[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
